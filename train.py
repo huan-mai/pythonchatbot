dd126fb4644c350c6d56a252932c1f002650d47c
huan-mai <54669881+huan-mai@users.noreply.github.com>
2019-08-30 11:15:02 +0700

    Update train.py

--- train.py
+++ train.py
@@ -78,11 +78,11 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-try:
-    model.load("model.tflearn")
-except:
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-    model.save("model.tflearn")
+#try:
+#    model.load("model.tflearn")
+#except:
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
 
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
