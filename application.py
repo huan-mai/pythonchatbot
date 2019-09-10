dc6641b75f6774da8823354f511a5b0c4a956465
huanmai-kms <55089709+huanmai-kms@users.noreply.github.com>
2019-09-10 13:34:45 +0700

    Update application.py

--- application.py
+++ application.py
@@ -4,8 +4,9 @@ import json
 from flask import Flask, request
 app = Flask(__name__)
 
-app_id = os.environ.get('APP_ID')
-app_secret = os.environ.get('APP_SECRET') 
+app_id = "ed163f1f-e7ce-4940-bda9-e5e1987aab72" 
+app_secret = "B3ZRkqum-a3WdU3*grM2NInP@FH*ay8z" 
+
 
 bot = skype_chatbot.SkypeBot(app_id, app_secret)
 
@@ -17,10 +18,11 @@ def hello():
 def webhook():
     try:
         ml
-    except NameError:    
+    except NameError:
         from train import Train
         ml = Train()
         ml.start()
+    answer = ''    
     if request.method == 'POST':
         try:
             data = json.loads(request.data)
@@ -32,11 +34,12 @@ def webhook():
             text = data['text']
 
             bot.send_message(bot_id, bot_name, recipient, service, sender, 'You said: "{}" and my answer: "{}"'.format(text, ml.answer(text)))
-
         except Exception as e:
             print(e)
-
-    return 'Code: 200. {}'.format(ml.answer('Hi'))
+    if request.method == 'GET':
+        question = request.args.get('q');
+        answer = ml.answer(question if question else 'Hi');
+    return 'Code: 200. {}'.format(answer)
 
 @app.route('/api/train', methods=['GET'])
 def train():
@@ -45,5 +48,5 @@ def train():
     try:
         ml.training()
         return "Train is completed"
-    except:
-        return "Traing is failed"
+    except Exception as e:
+        return "Traing is failed {}".format(str(e))
