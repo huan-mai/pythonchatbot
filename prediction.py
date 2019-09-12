74bedb1788a0ccd7cc575cee03ec27aa5f921630
huan-mai <54669881+huan-mai@users.noreply.github.com>
2019-09-12 11:25:07 +0700

    Refactor and add doc

--- prediction.py
+++ prediction.py
@@ -1,9 +1,5 @@
 import os
 import nltk
-nltk.download('punkt')
-from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer()
-
 import numpy
 import tflearn
 import tensorflow
@@ -11,15 +7,18 @@ import random
 import json
 import pickle
 
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
+nltk.download('punkt')
+
 ERROR_THRESHOLD = 0.25
-class Prediction(object):
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
-    context =  {}
-    def __init__(self, model_dir = '.'):
-        self.MODEL_DIR = model_dir if model_dir else '.'
+class Prediction:
+    # This class is mainly used to predict the answer from user's question. 
+    # It need below informations:
+    # - data.pickle file which store all preprocessing data from training step
+    # - input file or intent file (its name is stored as part of data.pickle file)
+    # - model.tflearn - training model from training step
 
     def load_model(self):
         if hasattr(self, 'data'):
@@ -41,6 +40,15 @@ class Prediction(object):
         net = tflearn.regression(net)
         self.model = tflearn.DNN(net)
         self.model.load("{}/model.tflearn".format(self.MODEL_DIR))
+
+    def __init__(self, model_dir = '.'):
+        self.MODEL_DIR = model_dir if model_dir else '.'
+        self.words = []
+        self.labels = []
+        self.docs_x = []
+        self.docs_y = []
+        self.context =  {}
+        self.load_model()
     
     def bag_of_words(self, s):
         bag = [0 for _ in range(len(self.words))]
@@ -48,7 +56,7 @@ class Prediction(object):
         s_words = nltk.word_tokenize(s)
         s_words = [stemmer.stem(word.lower()) for word in s_words]
 
-        print('questions: {}'.format(s_words));
+        print('questions: {}'.format(s_words))
         for se in s_words:
             for i, w in enumerate(self.words):
                 if w == se:
