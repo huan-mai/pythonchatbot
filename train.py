d6ccadd3999f9488623f90c668f9a6a6dab1f69b
huan-mai <54669881+huan-mai@users.noreply.github.com>
2019-09-12 11:25:42 +0700

    Refactor and add doc

--- train.py
+++ train.py
@@ -73,10 +73,11 @@ class Train:
         net = tflearn.fully_connected(net, 8)
         # turn result to probability rather than a number for classification: softmax, relu, sigmoid
         net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-
+        
+        # You have option to tune you training by setting different option e.g learning_rate
+        # For example: net = tflearn.regression(net, optimizer='sgd', learning_rate=0.01, loss='mean_square')
         net = tflearn.regression(net)
-        # net = tflearn.regression(net, optimizer='sgd', learning_rate=0.01, loss='mean_square')
-
+        
         model = tflearn.DNN(net)
         model.fit(training, output, n_epoch=200, batch_size=2, show_metric=True)
         model.save("{}/model.tflearn".format(self.model_dir))
